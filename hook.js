3d9277062219ba4750ca2cdd7ed22250e8f03ce0
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-03-09 10:43:41 -0400

    push to manifolds slack

--- hook.js
+++ hook.js
@@ -1,12 +1,41 @@
 #!/usr/bin/env node
 const { IncomingWebhook } = require('@slack/webhook');
+const read = require('@commitlint/read');
+var SlackWebhook = require('slack-webhook')
+
+var slack = new SlackWebhook('https://hooks.slack.com/services/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', {
+  defaults: {
+    username: 'git',
+    channel: '#precommit',
+    icon_emoji: ':robot_face:'
+  }
+})  
+
+
+
+read({edit: true})
+    .then(messages => console.log(messages))
+    .then(slack.send(messages))
+
 // const url = process.env.SLACK_WEBHOOK_URL;
- const url = 'https://hooks.slack.com/services/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'
-const webhook = new IncomingWebhook(url);
- 
-// Send the notification
-(async () => {
-  await webhook.send({
-    text: 'pre-commit caught by hook.js',
-  });
-})(); 
+// //  const url = 'https://hooks.slack.com/services/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'
+// const webhook = new IncomingWebhook(url);
+//  let messages
+// // Send the notification
+// (async () => {
+//   // Read last edited commit message
+// read({edit: true})
+//   .then(messages => send(messages))
+//   .then(
+//     webhook.send({
+//       text: msg
+//     })
+//   )
+// })(); 
+
+// // function send(msg){
+// //   console.log(msg)
+// //   webhook.send({
+// //     text: msg
+// //   })
+// // }
